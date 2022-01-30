538db289e5c0bb25c0f77441b528e403007b5e64
ruslan <ruslangr@yahoo.com>
2022-01-30 16:36:29 +0200

    CRUD 3.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Answer;
 use App\Entity\Question;
+use App\Entity\Topic;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -22,10 +25,12 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
 //        return parent::index();
-        $url = $this->adminUrlGenerator
+        /*$url = $this->adminUrlGenerator
             ->setController(QuestionCrudController::class)
             ->generateUrl();
-        return $this->redirect($url);
+        return $this->redirect($url);*/
+
+        return $this->render('my-dashboard.html.twig');
 
     }
 
@@ -40,7 +45,10 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
 //        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Question', 'fas fa-list', Question::class);
+        yield MenuItem::linkToCrud('Questions', 'fas fa-question-circle', Question::class);
+        yield MenuItem::linkToCrud('Answers', 'fas fa-comments', Answer::class);
+        yield MenuItem::linkToCrud('Topics', 'fas fa-folder', Topic::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
 
         /*return [
             MenuItem::linkToRoute('The Label', 'fa ...', 'admin'),
