3dca81f2de94045e002b5d175f6ca1d837cee4ce
ruslan <ruslangr@yahoo.com>
2022-08-25 15:57:15 +0300

    4. Global vs CRUD-Specific Configuration. Enable Show method globally and disable locally.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,9 @@ use App\Entity\Answer;
 use App\Entity\Question;
 use App\Entity\Topic;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -57,4 +60,12 @@ class DashboardController extends AbstractDashboardController
         ];*/
 
     }
+
+    public function configureActions():Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ;
+    }
+
 }
