c47c60865e9ce5d7ccc4c7091682ef0c3b3be0a0
ruslan <ruslangr@yahoo.com>
2022-08-26 15:32:12 +0300

    7. Configuring Fields. symfony guess about type.

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +13,15 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
+       /* return [
             IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
-        ];
+        ]; // OR :  */
+        yield Field::new('id');
     }
-    */
+
 }
