5df3f8f0e3bfeddb786a1b21b333ca060d4f0b54
ruslan <ruslangr@yahoo.com>
2022-08-26 11:30:50 +0300

    5. Controlling the Dashboard Menu. add usewr menu items

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,11 +11,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -70,5 +72,20 @@ class DashboardController extends AbstractDashboardController
         return parent::configureActions()
             ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
+
+    public function configureUserMenu(UserInterface $user):UserMenu
+    {
+        if(!$user instanceof User){
+            throw new \Exception('wrong user');
+        }
+        return parent::configureUserMenu($user)
+            ->setAvatarUrl($user->getAvatarUrl())
+            ->setMenuItems([
+                MenuItem::linkToUrl('My Profile', 'fas fa-user', $this->generateUrl(
+                    'app_profile_show'
+                ))
+                           ]);
+    }
+
 
 }
