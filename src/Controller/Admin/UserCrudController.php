6f6a04a3cf462d3b1c98f34676515f197d1464bc
ruslan <ruslangr@yahoo.com>
2022-08-26 23:55:45 +0300

    11. Controlling the "Formatted Value". Avatar improves for diff pages

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -34,15 +34,17 @@ class UserCrudController extends AbstractCrudController
         yield IdField::new('id')
             ->onlyOnIndex();
         yield AvatarField::new('avatar')
-        ->formatValue(static function($value, User $user){
-            return $user->getAvatarUrl();
-        });
+        ->formatValue(static function($value, ?User $user){
+            return $user?->getAvatarUrl();
+        })
+        ->hideOnForm();
 
         yield ImageField::new('avatar')
         ->setBasePath('uploads/avatars')
         ->setUploadDir('public/uploads/avatars')
         ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
 //        ->setFormTypeOption('upload_new', function()) // Add login to save in S3 service
+        ->onlyOnForms()
         ;
         yield EmailField::new('email');
         yield TextField::new('fullName') //it works because User::getFullName exists
