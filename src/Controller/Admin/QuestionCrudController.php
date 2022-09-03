b49bec60b1f9160b50c46e437b7b5a7c030c833f
ruslan <ruslangr@yahoo.com>
2022-09-03 12:10:11 +0300

    36. True Custom Action. add button for custom method

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -23,87 +23,87 @@ use function Sodium\add;
 #[IsGranted('ROLE_MODERATOR')]
 class QuestionCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
+    public static function getEntityFqcn():string
     {
         return Question::class;
     }
 
 
-    public function configureFields(string $pageName): iterable
+    public function configureFields(string $pageName):iterable
     {
         yield IdField::new('id')
             ->onlyOnIndex();
         yield Field::new('slug')
             ->hideOnIndex()
-        ->setFormTypeOption(
-            'disabled',
-            $pageName !== Crud::PAGE_NEW
-        );
+            ->setFormTypeOption(
+                'disabled',
+                $pageName !== Crud::PAGE_NEW
+            );
 
         yield Field::new('name')
-        ->setSortable(false);
+            ->setSortable(false);
         yield AssociationField::new('topic');
 //        yield TextEditorField::new('question')
         yield TextareaField::new('question')
             ->hideOnIndex()
-        ->setFormTypeOptions([
-            'row_attr' => [
-                'data-controller' => 'snarkdown'
-            ],
-            'attr' => [
-                'data-snarkdown-target' => 'input',
-                'data-action' => 'snarkdown#render'
-            ]
-                             ])
-        ->setHelp('Preview: ');
+            ->setFormTypeOptions([
+                                     'row_attr' => [
+                                         'data-controller' => 'snarkdown'
+                                     ],
+                                     'attr'     => [
+                                         'data-snarkdown-target' => 'input',
+                                         'data-action'           => 'snarkdown#render'
+                                     ]
+                                 ])
+            ->setHelp('Preview: ');
         yield VotesField::new('votes', 'Total votes')
             ->setTextAlign('right')
-        ->setPermission('ROLE_SUPER_ADMIN');
+            ->setPermission('ROLE_SUPER_ADMIN');
 
         yield AssociationField::new('askedBy')
-        ->autocomplete()
-        ->formatValue(static function($value, ?Question $question){
-            if(!$user = $question->getAskedBy()){
-                return null;
-            }
+            ->autocomplete()
+            ->formatValue(static function ($value, ?Question $question) {
+                if (!$user = $question->getAskedBy()) {
+                    return null;
+                }
 
-            return sprintf('%s&nbsp;(%s)', $user->getEmail(), $user->getQuestions()->count());
-        })
-        ->setQueryBuilder(function (QueryBuilder $queryBuilder){
-            $queryBuilder->andWhere('entity.enabled = :enabled')
-            ->setParameter('enabled', true);
-        });
+                return sprintf('%s&nbsp;(%s)', $user->getEmail(), $user->getQuestions()->count());
+            })
+            ->setQueryBuilder(function (QueryBuilder $queryBuilder) {
+                $queryBuilder->andWhere('entity.enabled = :enabled')
+                    ->setParameter('enabled', true);
+            });
         yield AssociationField::new('answers')
 //        ->setFormTypeOption('choice_label', 'id') // does not work with autocomplete
-        ->setFormTypeOption('by_reference', false)
-        ->autocomplete();
+            ->setFormTypeOption('by_reference', false)
+            ->autocomplete();
 
         yield Field::new('createdAt')
-        ->hideOnForm();
+            ->hideOnForm();
         yield AssociationField::new('updatedBy')
-        ->onlyOnDetail() ;
+            ->onlyOnDetail();
 
         yield Field::new('isApproved');
     }
 
-/*    public function configureCrud(Crud $crud): Crud
-    {
-        return parent::configureCrud($crud)
-            ->setDefaultSort([
-                'askedBy.enabled' => 'DESC',
-                'createdAt' => 'DESC',
-            ]);
-    }*/
+    /*    public function configureCrud(Crud $crud): Crud
+        {
+            return parent::configureCrud($crud)
+                ->setDefaultSort([
+                    'askedBy.enabled' => 'DESC',
+                    'createdAt' => 'DESC',
+                ]);
+        }*/
     public function configureCrud(Crud $crud):Crud
     {
         return parent::configureCrud($crud)
             ->setDefaultSort([
-                'askedBy.enabled' => 'DESC',
-                'createdAt' => 'DESC',
+                                 'askedBy.enabled' => 'DESC',
+                                 'createdAt'       => 'DESC',
                              ]);
     }
 
-    public function configureActions(Actions $actions): Actions
+    public function configureActions(Actions $actions):Actions
     {
         /*$viewAction = Action::new('view')
         ->linkToUrl(function (Question $question){
@@ -116,9 +116,9 @@ class QuestionCrudController extends AbstractCrudController
         ->setLabel('View on site');*/
 
         //Second way
-        $viewAction = function() {
+        $viewAction = function () {
             return Action::new('view')
-                ->linkToUrl(function(Question $question) {
+                ->linkToUrl(function (Question $question) {
                     return $this->generateUrl('app_question_show', [
                         'slug' => $question->getSlug(),
                     ]);
@@ -126,9 +126,16 @@ class QuestionCrudController extends AbstractCrudController
                 ->setIcon('fa fa-eye')
                 ->setLabel('View on site');
         };
+//--
+        $approveAction = Action::new('approve')
+            ->addCssClass('btn btn-success')
+            ->setIcon('fa fa-check-circle')
+            ->displayAsButton()
+            ->setTemplatePath('admin/approve_action.html.twig')
+            ->linkToCrudAction('approve');
 
         return parent::configureActions($actions)
-            ->update(Crud::PAGE_INDEX, Action::DELETE, static function(Action $action) {
+            ->update(Crud::PAGE_INDEX, Action::DELETE, static function (Action $action) {
                 $action->displayIf(static function (Question $question) {
 //                    return !$question->getIsApproved();
                     return true;
@@ -142,9 +149,9 @@ class QuestionCrudController extends AbstractCrudController
             ->setPermission(Action::NEW, 'ROLE_SUPER_ADMIN')
             ->setPermission(Action::DELETE, 'ROLE_SUPER_ADMIN')
             ->setPermission(Action::BATCH_DELETE, 'ROLE_SUPER_ADMIN')
-
             ->add(Crud::PAGE_DETAIL, $viewAction()->addCssClass('btn btn-success'))
-            ->add(Crud::PAGE_INDEX, $viewAction());
+            ->add(Crud::PAGE_INDEX, $viewAction())
+            ->add(Crud::PAGE_DETAIL, $approveAction);
 //        ->add(Crud::PAGE_DETAIL, $viewAction)
 //        ->add(Crud::PAGE_INDEX, $viewAction);
     }
@@ -175,7 +182,7 @@ class QuestionCrudController extends AbstractCrudController
      */
     public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance):void
     {
-        if($entityInstance->getIsApproved()){
+        if ($entityInstance->getIsApproved()) {
             throw new \Exception('Deleting approved questions is forbidden!');
         }
 
